06d4f55dbe69d40f2c9512f9f70b6a434515c98a
mask0fdarkness <114158895+mask0fdarkness@users.noreply.github.com>
2022-09-22 17:39:04 +0500

    Add files via upload

---
+++ svetofor.ino
@@ -0,0 +1,702 @@
+int but1 =7;
+int m1 = 2;
+int m2 = 3;
+int m3 = 4;
+int l1 = 9;
+int l2 = 10;
+int p = 0;
+int k = 5;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(but1,INPUT);
+  pinMode(2,OUTPUT);
+  pinMode(8,INPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(5,OUTPUT);
+  digitalWrite(l1,HIGH);
+}
+void loop()
+{
+  digitalWrite(m1,HIGH);
+  for (int i = 0; i < 100; i++) {
+    Serial.println(p);
+    delay(30);
+      if (digitalRead(but1)== HIGH) {
+        delay(6);
+        if (digitalRead(but1) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+          }
+          }
+          
+       if (digitalRead(8)== HIGH) {
+         delay(6);
+         if (digitalRead(8) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+         }
+       }
+     
+   }
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+  for (int i = 0; i < 100; i++) {
+    Serial.println(p);
+    delay(30);
+      if (digitalRead(but1)== HIGH) {
+        delay(6);
+        if (digitalRead(but1) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+            for (int i = 0; i < 100; i++) {
+    Serial.println(p);
+    delay(30);
+      if (digitalRead(but1)== HIGH) {
+        delay(6);
+        if (digitalRead(but1) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+          }
+          }
+          
+       if (digitalRead(8)== HIGH) {
+         delay(6);
+         if (digitalRead(8) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+         }
+       }
+     
+   }
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+        }
+      }
+      if (digitalRead(8)== HIGH) {
+        delay(6);
+        if (digitalRead(8) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+            for (int i = 0; i < 100; i++) {
+    Serial.println(p);
+    delay(30);
+      if (digitalRead(but1)== HIGH) {
+        delay(6);
+        if (digitalRead(but1) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+          }
+          }
+          
+       if (digitalRead(8)== HIGH) {
+         delay(6);
+         if (digitalRead(8) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+         }
+       }
+     
+   }
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+        }
+      }
+    }
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,HIGH);
+  for (int i = 0; i < 100; i++) {
+    Serial.println(p);
+    delay(30);
+      if (digitalRead(but1)== HIGH) {
+        delay(6);
+        if (digitalRead(but1) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+            for (int i = 0; i < 100; i++) {
+    Serial.println(p);
+    delay(30);
+      if (digitalRead(but1)== HIGH) {
+        delay(6);
+        if (digitalRead(but1) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+          }
+          }
+          
+       if (digitalRead(8)== HIGH) {
+         delay(6);
+         if (digitalRead(8) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+         }
+       }
+     
+   }
+  
+        }
+      }
+      if (digitalRead(8)== HIGH) {
+        delay(6);
+        if (digitalRead(8) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+            for (int i = 0; i < 100; i++) {
+    Serial.println(p);
+    delay(30);
+      if (digitalRead(but1)== HIGH) {
+        delay(6);
+        if (digitalRead(but1) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+          }
+          }
+          
+       if (digitalRead(8)== HIGH) {
+         delay(6);
+         if (digitalRead(8) ==HIGH) {
+          digitalWrite(k,HIGH);
+          for (int i = 0; i < 50; i++) {
+            delay(30);
+          }
+          digitalWrite(m1,LOW);
+          digitalWrite(m2,HIGH);
+          delay(3000);
+          digitalWrite(m2,LOW);
+          digitalWrite(m3,HIGH);
+          delay(1000);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          delay(500);
+          digitalWrite(m3,LOW);
+          delay(500);
+          digitalWrite(m3,HIGH);
+          digitalWrite(k,LOW);
+          digitalWrite(l1,LOW);
+          digitalWrite(l2,HIGH);
+          delay(5000);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          delay(500);
+          digitalWrite(l2,HIGH);
+          delay(500);
+          digitalWrite(l2,LOW);
+          digitalWrite(l1,HIGH);
+          digitalWrite(m3,LOW);
+          digitalWrite(m1,HIGH);
+         }
+       }
+     
+   }
+  
+        }
+      }
+    }
+    digitalWrite(m3,LOW);
+}
